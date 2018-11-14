0440ce44d18391d796645fa49fb17ffcb92a339b
Wade Stewart <stew.wade@gmail.com>
2018-11-14 18:55:26 -0500

    rendering json to / for all artworks and /artwork for individual pieces

--- app.js
+++ app.js
@@ -6,14 +6,29 @@ require('dotenv').config()
 app.set('port', process.env.PORT || 3001)
 
 app.listen(app.get('port'), () => console.log('live on port 3001'))
-// console.log(process.env.apiKey)
 app.get('/', (req, res) => {
+    fetch(`https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getOnDisplay&access_token=${process.env.apiKey}&per_page=2`)
+        .then(res => res.json())
+        .then(data => {
+            res.send(data.objects)
+          })
+        .catch(err => console.log(err))
+})
+
+app.get('/artwork', (req, res) => {
     fetch(`https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getOnDisplay&access_token=${process.env.apiKey}&per_page=1`)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
-            res.send(data)
-          })
-        // .then(res.send('Hello World'))
-        .catch(err => console.log(err))
+            // I need to write a function here to handle the 2nd API call
+            data.objects.forEach((art) => {
+                let id = art.id
+                fetch(`https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getInfo&access_token=${process.env.apiKey}&id=${id}`)
+                .then(res => res.json())
+                .then(data => {
+                    res.send(data)
+                })
+            })
+        })
+    
 })
+
